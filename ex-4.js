299851ad50cdeaae0d372c2023070a5882b30c27
T <itthi91@gmail.com>
2023-12-06 13:02:45 +0700

    fix spacing ex7

--- ex-4.js
+++ ex-4.js
@@ -3,16 +3,16 @@
 let lightBulbStatus = "On";
 
 //Start coding here
-switch(lightBulbStatus) {
-    case("On"):
+switch (lightBulbStatus) {
+  case "On":
     console.log("Light bulb is On.");
     break;
-    case("Off"):
+  case "Off":
     console.log("Light bulb is Off.");
     break;
-    case("Broken"):
-    console.log("Light bulb is Broken.")
+  case "Broken":
+    console.log("Light bulb is Broken.");
     break;
-    default: 
-    console.log(`Please choose the correct input (On/Off)`)
+  default:
+    console.log(`Please choose the correct input (On/Off)`);
 }
--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,4 @@
 //Start coding here
 const greeting = () => console.log("Hello ! 😁");
-
 greeting();
-
 console.log("This line will be executed without any error.");
